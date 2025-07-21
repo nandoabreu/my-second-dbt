621a3fe496b42904c29ff14d901f90e45679e24c
Fernando Abreu <49952773+nandoabreu@users.noreply.github.com>
2025-07-21 21:50:14 +0100

    Transform dim_customers with country names

--- the_project/models/marts/dim_customers.sql
+++ the_project/models/marts/dim_customers.sql
@@ -3,13 +3,15 @@ WITH staged_customers AS (SELECT * FROM {{ ref("stg_customers") }})
    , customer_orders AS (
         SELECT c.customer_id
              , c.gender
-             , c.country
              , c.city
+             , c.country AS country_code
+             , cc.name AS country_name
              , MIN(o.order_approved_at) AS first_order_date
              , MAX(o.order_approved_at) AS most_recent_order_date
              , COUNT(o.order_id) AS number_of_orders
         FROM staged_orders o
             INNER JOIN staged_customers c USING (customer_id)
-        GROUP BY c.customer_id, c.gender, c.country, c.city, c.email
+            LEFT JOIN {{ ref('ref_countries') }} cc ON c.country = cc.code
+        GROUP BY c.customer_id, c.gender, c.city, c.country, cc.name, c.email
    )
 SELECT * FROM customer_orders
